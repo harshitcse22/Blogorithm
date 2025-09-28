3527788747b2864bafe489a26b81c1893d60e4f4
Harshit <hktiwari980@gmail.com>
2025-09-28 20:13:51 +0530

    BlogList Created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,10 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
-       <img onClick={() =>navigate('/')} src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer'/>
+       <div onClick={() =>navigate('/')} className='flex items-center gap-2 cursor-pointer'>
+         <img src="/favicon.svg" alt="logo" className='w-8 h-8'/>
+         <span className='text-xl font-bold'>Blogorithm</span>
+       </div>
         <button onClick={()=> navigate('/admin')} className='flex items-center gap-2 rounded-full text-sm
           cursor-pointer bg-primary text-white px-10 py-2.5'>
            Login

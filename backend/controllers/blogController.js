c04a821420ae637181822fe1822f8e5ce9b6c1c7
Harshit <hktiwari980@gmail.com>
2025-11-08 18:26:44 +0530

    backend connected, db created

---
+++ backend/controllers/blogController.js
@@ -0,0 +1,7 @@
+export const addBlog = async (req, res) =>{
+    try{
+    //    const {title, subTitle, description, category, isPublished} = JSON.parse
+    }catch(error){
+
+    }
+}
--- frontend/src/pages/admin/Layout.jsx
+++ frontend/src/pages/admin/Layout.jsx
@@ -9,14 +9,21 @@ const Layout = () => {
 
      const logout = () => {
          navigate('/')
-     }
+     }  //className='w-8 h-8'/>
+        // <span className='text-xl font-bold'>Blogorithm</span>
 
   return (
     <>
     <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12
         border-b border-gray-200'>
-       <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer'
-         onClick={() => Navigate('/')}/>
+        
+       <div className='flex items-center gap-2 cursor-pointer' onClick={() => navigate('/')}>
+         <img src="/favicon.svg" 
+           alt="logo" 
+           className='w-8 h-8'/>
+         <span className='text-xl font-bold'>Blogorithm</span>
+       </div>
+
          <button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white 
          rounded-full cursor-pointer'>Logout</button>
     </div>

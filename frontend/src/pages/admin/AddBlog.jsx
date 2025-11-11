86228939b18588eb59ad78ab0f60f6935e82e367
Harshit <hktiwari980@gmail.com>
2025-11-11 17:54:34 +0530

    blog generation with AI implemented

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -3,11 +3,13 @@ import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill';
 import { useAppContext } from '../../context/AppContext';
 import toast from 'react-hot-toast';
+import { parse } from 'marked';
 
 const AddBlog = () => {
 
      const {axios} = useAppContext()
      const [isAdding, setIsAdding] = useState(false)
+     const [loading, setloading] = useState(false)
 
     const editorRef = useRef(null);
     const quillRef = useRef(null)
@@ -19,7 +21,21 @@ const AddBlog = () => {
      const [isPublished, setIsPublished] = useState(false);
 
      const generateContent = async ()=>{
-        
+         if(!title) return toast.error('Please enter a title')
+
+            try {
+               setloading(true);
+               const {data} = await axios.post('/api/blog/generate', {prompt: title})
+               if(data.success){
+                  quillRef.current.root.innerHTML = parse(data.content)
+               }else{
+                  toast.error(data.message)
+               }
+            }catch(error){
+               toast.error(error.message)
+            }finally{
+                setloading(false)
+            }
      }
 
      const onSubmitHandler = async (e) =>{
@@ -86,7 +102,7 @@ const AddBlog = () => {
               <p className='mt-4'>Blog Description</p>
               <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
                  <div ref={editorRef}></div>
-                 <button type='button' onClick={generateContent}
+                 <button disabled={loading} type='button' onClick={generateContent}
                  className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5
                    rounded hover:underline cursor-pointer'>
                     Generate with AI
